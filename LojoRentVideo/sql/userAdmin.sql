e0197264dcecd290631593e7b09ec2b626cacfeb
antoniojoselojoojeda <ajloinformatico@gmail.com>
2021-02-06 20:06:28 +0100

    bye

--- LojoRentVideo/sql/userAdmin.sql
+++
@@ -1,4 +0,0 @@
-drop user if exists 'admin'@'localhost';
-create user 'admin'@'localhost' identified by 'admin';
-grant all privileges on instituto to 'admin'@'localhost';
-flush privileges ;
--- README.md
+++
@@ -1,3 +0,0 @@
-# LojoVideoclub
-Video rent Web App using DAO pattern with JAVA 11, MySQL, Tomcat 8.5, Json Data and  Ajax with jQuery.
-## Actually building
